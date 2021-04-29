a4c13a9edb331e1f3c555ec4f90df2ee9805ab81
Dontag <nishadsur@gmail.com>
2021-04-29 20:41:33 +0530

    first commit

---
+++ src/screens/home/dashboard.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import { StyleSheet, Text, View } from 'react-native';
+
+const Dashboard = () => {
+    return (
+        <View>
+            <Text>This is Dashboard</Text>
+        </View>
+    )
+}
+
+const styles = StyleSheet.create({})
+export default Dashboard;
